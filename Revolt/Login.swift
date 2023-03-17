0ea82e98550a5df3cac4f987947711dc7c4ed20c
Zomatree <zomatree@Pauls-Mac-mini.local>
2023-03-17 23:23:25 +0000

    login page framework

---
+++ Revolt/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Revolt
+//
+//  Created by Zomatree on 17/03/2023.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
