df367431048c62b34d68b07d1261c9a30c1c0508
Zomatree <me@zomatree.live>
2024-06-18 04:24:23 +0100

    rework http errors and dedup `.req` code

--- Revolt/Api/Http.swift
+++ Revolt/Api/Http.swift
@@ -10,6 +10,12 @@ import Alamofire
 import Types
 import os
 
+enum RevoltError: Error {
+    case Alamofire(AFError)
+    case HTTPError(String?, Int)
+    case JSONDecoding(any Error)
+}
+
 struct HTTPClient {
     var token: String?
     var baseURL: String
@@ -25,6 +31,42 @@ struct HTTPClient {
         self.logger = Logger(subsystem: "chat.revolt.Revolt", category: "HTTP")
     }
 
+    func innerReq<
+        I: Encodable
+    >(
+        method: HTTPMethod,
+        route: String,
+        parameters: I? = nil as Int?,
+        encoder: ParameterEncoder = JSONParameterEncoder.default
+    ) async -> Result<DataResponse<String, AFError>, RevoltError> {
+        let req = self.session.request(
+            "\(baseURL)\(route)",
+            method: method,
+            parameters: parameters,
+            encoder: encoder,
+            headers: token.map({ HTTPHeaders(dictionaryLiteral: ("x-session-token", $0)) })
+        )
+        
+        let response = await req.serializingString()
+            .response
+        
+        let code = response.response?.statusCode ?? 500
+        
+        do {
+            let resp = try response.result.get()
+            logger.debug("OK:    Received response \(code) for route \(method.rawValue) \(baseURL)\(route) with result \(resp)")
+        } catch {
+            logger.debug("Error: Received response \(code) for route \(method.rawValue) \(baseURL)\(route) with result \(response.error)")
+        }
+        
+        if ![200, 201, 202, 203, 204, 205, 206, 207, 208, 226].contains(code) {
+            return .failure(.HTTPError(response.value, code))
+        }
+     
+        return .success(response)
+        
+    }
+    
     func req<
         I: Encodable,
         O: Decodable
@@ -33,32 +75,18 @@ struct HTTPClient {
         route: String,
         parameters: I? = nil as Int?,
         encoder: ParameterEncoder = JSONParameterEncoder.default
-    ) async -> Result<O, AFError> {
-        let req = self.session.request(
-            "\(baseURL)\(route)",
-            method: method,
-            parameters: parameters,
-            encoder: encoder,
-            headers: token.map({ HTTPHeaders(dictionaryLiteral: ("x-session-token", $0)) })
-        )
-    
-        let response = await req.serializingString()
-            .response
-        
-        let code = response.response?.statusCode ?? 500
-        
-        do {
-            let resp = try response.result.get()
-            logger.debug("Received response \(code) for route \(method.rawValue) \(baseURL)\(route) with result \(resp)")
-        } catch {
-            logger.debug("Received response \(code) for route \(method.rawValue) \(baseURL)\(route) with bad result \(response.error)")
+    ) async -> Result<O, RevoltError> {
+        return await innerReq(method: method, route: route, parameters: parameters, encoder: encoder).flatMap { response in
+            response.result
+                .mapError(RevoltError.Alamofire)
+                .flatMap {
+                    do {
+                        return .success(try JSONDecoder().decode(O.self, from: $0.data(using: .utf8)!))
+                    } catch {
+                        return .failure(.JSONDecoding(error))
+                    }
+                }
         }
-
-        if ![200, 201, 202, 203, 204, 205, 206, 207, 208, 226].contains(code) {
-            return Result.failure(AFError.responseSerializationFailed(reason: .inputFileNil))
-        }
-               
-        return response.result.map({ b in try! JSONDecoder().decode(O.self, from: b.data(using: .utf8)!) })
     }
 
     func req<
@@ -68,39 +96,23 @@ struct HTTPClient {
         route: String,
         parameters: I? = nil as Int?,
         encoder: ParameterEncoder = JSONParameterEncoder.default
-    ) async -> Result<EmptyResponse, AFError> {
-        let req = self.session.request(
-            "\(baseURL)\(route)",
-            method: method,
-            parameters: parameters,
-            encoder: encoder,
-            headers: token.map({ HTTPHeaders(dictionaryLiteral: ("x-session-token", $0)) })
-        )
-        
-        let response = await req.serializingString()
-            .response
-        
-        let code = response.response?.statusCode ?? 500
-        
-        logger.debug("Received response \(code) for route \(method.rawValue) \(baseURL)\(route) with result \(try! response.result.get())")
-
-        
-        if ![200, 201, 202, 203, 204, 205, 206, 207, 208, 226].contains(code) {
-            return Result.failure(AFError.responseSerializationFailed(reason: .inputFileNil))
+    ) async -> Result<EmptyResponse, RevoltError> {
+        return await innerReq(method: method, route: route, parameters: parameters, encoder: encoder).flatMap { response in
+            response.result
+                .mapError(RevoltError.Alamofire)
+                .map { _ in EmptyResponse() }
         }
-        
-        return response.result.map({ _ in EmptyResponse() })
     }
     
-    func fetchSelf() async -> Result<User, AFError> {
+    func fetchSelf() async -> Result<User, RevoltError> {
         await req(method: .get, route: "/users/@me")
     }
 
-    func fetchApiInfo() async -> Result<ApiInfo, AFError> {
+    func fetchApiInfo() async -> Result<ApiInfo, RevoltError> {
         await req(method: .get, route: "/")
     }
     
-    func sendMessage(channel: String, replies: [ApiReply], content: String, attachments: [(Data, String)], nonce: String) async -> Result<Message, AFError> {
+    func sendMessage(channel: String, replies: [ApiReply], content: String, attachments: [(Data, String)], nonce: String) async -> Result<Message, RevoltError> {
         var attachmentIds: [String] = []
         
         for attachment in attachments {
@@ -112,30 +124,15 @@ struct HTTPClient {
         return await req(method: .post, route: "/channels/\(channel)/messages", parameters: SendMessage(replies: replies, content: content, attachments: attachmentIds))
     }
     
-    func fetchUser(user: String) async -> Result<User, AFError> {
+    func fetchUser(user: String) async -> Result<User, RevoltError> {
         await req(method: .get, route: "/users/\(user)")
     }
     
-    func deleteMessage(channel: String, message: String) async -> Result<EmptyResponse, AFError> {
-        let req = self.session.request(
-            "\(baseURL)/channels/\(channel)/messages/\(message)",
-            method: .delete,
-            headers: token.map({ HTTPHeaders(dictionaryLiteral: ("x-session-token", $0)) })
-        )
-        
-        let response = await req.serializingString()
-            .response
-        
-        let code = response.response?.statusCode
-        
-        if ![200, 201, 202, 203, 204, 205, 206, 207, 208, 226].contains(code) {
-            return Result.failure(AFError.responseSerializationFailed(reason: .inputFileNil))
-        }
-        
-        return response.result.map({ _ in EmptyResponse() })
+    func deleteMessage(channel: String, message: String) async -> Result<EmptyResponse, RevoltError> {
+        await req(method: .delete, route: "\(baseURL)/channels/\(channel)/messages/\(message)")
     }
     
-    func fetchHistory(channel: String, limit: Int, before: String?) async -> Result<FetchHistory, AFError> {
+    func fetchHistory(channel: String, limit: Int, before: String?) async -> Result<FetchHistory, RevoltError> {
         var url = "/channels/\(channel)/messages?limit=\(limit)&include_users=true"
         
         if let before = before {
@@ -145,19 +142,19 @@ struct HTTPClient {
         return await req(method: .get, route: url)
     }
     
-    func fetchMessage(channel: String, message: String) async -> Result<Message, AFError> {
+    func fetchMessage(channel: String, message: String) async -> Result<Message, RevoltError> {
         await req(method: .get, route: "/channels/\(channel)/messages/\(message)")
     }
     
-    func fetchDms() async -> Result<[Channel], AFError> {
+    func fetchDms() async -> Result<[Channel], RevoltError> {
         await req(method: .get, route: "/users/dms")
     }
     
-    func fetchProfile(user: String) async -> Result<Profile, AFError> {
+    func fetchProfile(user: String) async -> Result<Profile, RevoltError> {
         await req(method: .get, route: "/users/\(user)/profile")
     }
     
-    func uploadFile(data: Data, name: String, category: FileCategory) async -> Result<AutumnResponse, AFError> {
+    func uploadFile(data: Data, name: String, category: FileCategory) async -> Result<AutumnResponse, RevoltError> {
         let url = "\(apiInfo!.features.autumn.url)/\(category.rawValue)"
 
         return await session.upload(
@@ -167,105 +164,106 @@ struct HTTPClient {
             .serializingDecodable(decoder: JSONDecoder())
             .response
             .result
+            .mapError(RevoltError.Alamofire)
     }
     
-    func fetchSessions() async -> Result<[Types.Session], AFError> {
+    func fetchSessions() async -> Result<[Types.Session], RevoltError> {
         await req(method: .get, route: "/auth/session/all")
     }
     
-    func deleteSession(session: String) async -> Result<EmptyResponse, AFError> {
+    func deleteSession(session: String) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .delete, route: "/auth/session/\(session)")
     }
     
-    func joinServer(code: String) async -> Result<JoinResponse, AFError> {
+    func joinServer(code: String) async -> Result<JoinResponse, RevoltError> {
         await req(method: .post, route: "/invites/\(code)")
     }
     
-    func reportMessage(id: String, reason: ContentReportPayload.ContentReportReason, userContext: String) async -> Result<EmptyResponse, AFError> {
+    func reportMessage(id: String, reason: ContentReportPayload.ContentReportReason, userContext: String) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .post, route: "/safety/report", parameters: ContentReportPayload(type: .Message, contentId: id, reason: reason, userContext: userContext))
     }
     
-    func createAccount(email: String, password: String, invite: String?, captcha: String?) async -> Result<EmptyResponse, AFError> {
+    func createAccount(email: String, password: String, invite: String?, captcha: String?) async -> Result<EmptyResponse, RevoltError> {
         return await req(method: .post, route: "/auth/account/create", parameters: AccountCreatePayload(email: email, password: password, invite: invite, captcha: captcha))
     }
     
-    func createAccount_VerificationCode(code: String) async -> Result<AccountCreateVerifyResponse, AFError> {
+    func createAccount_VerificationCode(code: String) async -> Result<AccountCreateVerifyResponse, RevoltError> {
         await req(method: .post, route: "/auth/account/verify/\(code)")
     }
     
-    func createAccount_ResendVerification(email: String, captcha: String?) async -> Result<EmptyResponse, AFError> {
+    func createAccount_ResendVerification(email: String, captcha: String?) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .post, route: "/auth/account/reverify", parameters: ["email": email, "captcha": captcha])
     }
     
-    func sendResetPasswordEmail(email: String, captcha: String?) async -> Result<EmptyResponse, AFError> {
+    func sendResetPasswordEmail(email: String, captcha: String?) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .post, route: "/auth/account/reverify", parameters: ["email": email, "captcha": captcha])
     }
     
-    func resetPassword(token: String, password: String, removeSessions: Bool = false) async -> Result<EmptyResponse, AFError> {
+    func resetPassword(token: String, password: String, removeSessions: Bool = false) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .patch, route: "/auth/account/reset_password", parameters: PasswordResetPayload(token: token, password: password))
     }
     
-    func checkOnboarding() async -> Result<OnboardingStatusResponse, AFError> {
+    func checkOnboarding() async -> Result<OnboardingStatusResponse, RevoltError> {
         await req(method: .get, route: "/onboard/hello")
     }
     
-    func completeOnboarding(username: String) async -> Result<EmptyResponse, AFError> {
+    func completeOnboarding(username: String) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .post, route: "/onboard/complete", parameters: ["username": username])
     }
     
-    func acceptFriendRequest(user: String) async -> Result<User, AFError> {
+    func acceptFriendRequest(user: String) async -> Result<User, RevoltError> {
         await req(method: .put, route: "/users/\(user)/friend")
     }
     
-    func removeFriend(user: String) async -> Result<User, AFError> {
+    func removeFriend(user: String) async -> Result<User, RevoltError> {
         await req(method: .delete, route: "/users/\(user)/friend")
     }
     
-    func blockUser(user: String) async -> Result<User, AFError> {
+    func blockUser(user: String) async -> Result<User, RevoltError> {
         await req(method: .put, route: "/users/\(user)/block")
     }
     
-    func unblockUser(user: String) async -> Result<User, AFError> {
+    func unblockUser(user: String) async -> Result<User, RevoltError> {
         await req(method: .delete, route: "/users/\(user)/block")
     }
     
-    func sendFriendRequest(username: String) async -> Result<User, AFError> {
+    func sendFriendRequest(username: String) async -> Result<User, RevoltError> {
         await req(method: .post, route: "/users/friend", parameters: ["username": username])
     }
     
-    func openDm(user: String) async -> Result<Channel, AFError> {
+    func openDm(user: String) async -> Result<Channel, RevoltError> {
         await req(method: .get, route: "/users/\(user)/dm")
     }
     
-    func fetchUnreads() async -> Result<[Unread], AFError> {
+    func fetchUnreads() async -> Result<[Unread], RevoltError> {
         await req(method: .get, route: "/sync/unreads")
     }
     
-    func ackMessage(channel: String, message: String) async -> Result<EmptyResponse, AFError> {
+    func ackMessage(channel: String, message: String) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .put, route: "/channels/\(channel)/ack/\(message)")
     }
     
-    func createGroup(name: String, users: [String]) async -> Result<Channel, AFError> {
+    func createGroup(name: String, users: [String]) async -> Result<Channel, RevoltError> {
         await req(method: .post, route: "/channels/create", parameters: GroupChannelCreate(name: name, users: users))
     }
     
-    func createInvite(channel: String) async -> Result<Invite, AFError> {
+    func createInvite(channel: String) async -> Result<Invite, RevoltError> {
         await req(method: .post, route: "/channels/\(channel)/invites")
     }
     
-    func fetchMember(server: String, member: String) async -> Result<Member, AFError> {
+    func fetchMember(server: String, member: String) async -> Result<Member, RevoltError> {
         await req(method: .get, route: "/servers/\(server)/members/\(member)")
     }
     
-    func editServer(server: String, edits: ServerEdit) async -> Result<Server, AFError> {
+    func editServer(server: String, edits: ServerEdit) async -> Result<Server, RevoltError> {
         await req(method: .patch, route: "/servers/\(server)", parameters: edits)
     }
     
-    func reactMessage(channel: String, message: String, emoji: String) async -> Result<EmptyResponse, AFError> {
+    func reactMessage(channel: String, message: String, emoji: String) async -> Result<EmptyResponse, RevoltError> {
         await req(method: .put, route: "/channels/\(channel)/messages/\(message)/reactions/\(emoji)")
     }
     
-    func editMessage(channel: String, message: String, edits: MessageEdit) async -> Result<Message, AFError> {
+    func editMessage(channel: String, message: String, edits: MessageEdit) async -> Result<Message, RevoltError> {
         await req(method: .patch, route: "/channels/\(channel)/messages/\(message)", parameters: edits)
     }
 }
