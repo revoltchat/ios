08df985834729a3705fd141c88942a7e536098d8
Zomatree <me@zomatree.live>
2024-08-05 19:21:29 +0100

    feat: render role colours in replies

--- Revolt/Components/MessageRenderer/MessageReply.swift
+++ Revolt/Components/MessageRenderer/MessageReply.swift
@@ -12,22 +12,32 @@ import Types
 struct MessageReplyView: View {
     @EnvironmentObject var viewState: ViewState
     
-    @Binding var mentions: [String]?
-    var channelScrollPosition: ChannelScrollController
     @State var dead: Bool = false
+
+    @Binding var mentions: [String]?
+    
+    var channelScrollPosition: ChannelScrollController
     var id: String
-    var channel: String
+    var server: Server?
+    var channel: Channel
     
     var body: some View {
         let message = viewState.messages[id]
+        
         if message != nil || dead {
-            InnerMessageReplyView(mentions: $mentions, channelScrollPosition: channelScrollPosition, message: message)
+            InnerMessageReplyView(mentions: $mentions, channelScrollPosition: channelScrollPosition, server: server, message: message)
         } else {
             if !viewState.loadingMessages.contains(id) {
                 let _ = Task {
                     do {
-                        let message = try await viewState.http.fetchMessage(channel: channel, message: id).get()
+                        let message = try await viewState.http.fetchMessage(channel: channel.id, message: id).get()
                         viewState.messages[id] = message
+                        
+                        if let server, viewState.members[server.id]?[message.author] == nil {
+                            if let member = try? await viewState.http.fetchMember(server: server.id, member: message.author).get() {
+                                viewState.members[server.id]?[message.author] = member
+                            }
+                        }
                     } catch {
                         dead = true
                     }
@@ -44,29 +54,37 @@ struct InnerMessageReplyView: View {
     
     @Binding var mentions: [String]?
     var channelScrollPosition: ChannelScrollController
+    var server: Server?
     var message: Message?
+    
+    func formatName(message: Message, author: User, member: Member?) -> String {
+        (mentions?.contains(message.author) == true ? "@" : "") + (message.masquerade?.name ?? member?.nickname ?? author.display_name ?? author.username)
+    }
     
     var body: some View {
         if let message = message {
+            let author = viewState.users[message.author] ?? User(id: "0", username: "Unknown User", discriminator: "0000")
+            let member = server.flatMap { viewState.members[$0.id] }.flatMap { $0[message.author] }
+
             HStack(spacing: 4) {
-                let author = viewState.users[message.author] ?? User(id: "0", username: "Unknown User", discriminator: "0000")
-                let member = viewState.currentServer.id.flatMap { viewState.members[$0] }.flatMap { $0[message.author] }
                 
                 Avatar(user: author, member: member, masquerade: message.masquerade, width: 16, height: 16)
                 
-                HStack(spacing: 0) {
-                    if mentions?.contains(message.author) == true {
-                        Text("@")
-                            .font(.caption)
-                    }
-                    
-                    Text(message.masquerade?.name ?? member?.nickname ?? author.display_name ?? author.username)
+                Text(formatName(message: message, author: author, member: member))
+                    .lineLimit(1)
+                    .foregroundStyle(member?.displayColour(theme: viewState.theme, server: server!) ?? AnyShapeStyle(viewState.theme.foreground.color))
+                    .font(.caption)
+                
+                if !(message.attachments?.isEmpty ?? true) {
+                    Text(Image(systemName: "doc.text.fill"))
                         .font(.caption)
+                        .foregroundStyle(viewState.theme.foreground2)
                 }
                 
                 if let content = message.content {
                     Text(content)
                         .font(.caption)
+                        .foregroundStyle(viewState.theme.foreground2)
                         .lineLimit(1)
                         .truncationMode(.tail)
                 }
