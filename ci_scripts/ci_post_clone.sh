81dab591a98e462d62b210f12d9d107148acaa08
Zomatree <me@zomatree.live>
2024-08-22 16:22:37 +0100

    ci: install sentry-cli

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,18 @@
-#
 //  ci_post_clone.sh
 //  Revolt
 //
 //  Created by Angelo on 22/08/2024.
 //
 
+// allow using macros
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+
+// install sentry-cli
+
+export INSTALL_DIR=$PWD
+
+if [[ $(command -v sentry-cli) == "" ]]; then
+    echo "Installing Sentry CLI"
+    curl -sL https://sentry.io/get-cli/ | bash
+fi
