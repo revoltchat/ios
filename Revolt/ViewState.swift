45958c8999906798071343e9def3f0a56189550c
Zomatree <me@zomatree.live>
2024-09-09 04:44:27 +0100

    fix: update api url to bypass cloudflare issues

--- Revolt/ViewState.swift
+++ Revolt/ViewState.swift
@@ -135,7 +135,7 @@ public class ViewState: ObservableObject {
     static var application: NSApplication? = nil
 #endif
 
-    var http: HTTPClient = HTTPClient(token: nil, baseURL: "https://revolt.chat/api")
+    var http: HTTPClient = HTTPClient(token: nil, baseURL: "https://app.revolt.chat/api")
     var ws: WebSocketStream? = nil
     var apiInfo: ApiInfo? = nil
     
