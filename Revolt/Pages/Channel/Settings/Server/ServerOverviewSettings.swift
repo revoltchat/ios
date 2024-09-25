43c999527b55f07c2592a2845617fe49ef75f5c3
Zomatree <me@zomatree.live>
2024-09-25 20:59:09 +0100

    feat: finish server overview settings

--- Revolt/Pages/Channel/Settings/Server/ServerOverviewSettings.swift
+++ Revolt/Pages/Channel/Settings/Server/ServerOverviewSettings.swift
@@ -13,13 +13,19 @@ import Types
 struct ServerOverviewSettings: View {
     @EnvironmentObject var viewState: ViewState
     
-    enum Icon: Equatable {
+    struct LocalFile: Equatable {
+        var content: Data
+        var filename: String
+    }
+    
+    enum SettingImage: Equatable {
         case remote(File?)
-        case local(Data)
+        case local(LocalFile?)
     }
     
     struct ServerSettingsValues: Equatable {
-        var icon: Icon
+        var icon: SettingImage
+        var banner: SettingImage
         var name: String
         var description: String
     }
@@ -27,58 +33,126 @@ struct ServerOverviewSettings: View {
     @State var initial: ServerSettingsValues
     @State var currentValues: ServerSettingsValues
     @State var showSaveButton: Bool = false
+    
     @State var showIconPhotoPicker: Bool = false
-    @State var serverIconPhoto: PhotosPickerItem?
+    @State var serverIconPhoto: PhotosPickerItem? = nil
+    
+    @State var showBannerPhotoPicker: Bool = false
+    @State var serverBannerPhoto: PhotosPickerItem? = nil
     
     @Binding var server: Server
     
-    @MainActor
-    static func fromState(viewState: ViewState, server s: Binding<Server>) -> Self {
+    init(server s: Binding<Server>) {
         let settings = ServerSettingsValues(
             icon: .remote(s.icon.wrappedValue),
+            banner: .remote(s.banner.wrappedValue),
             name: s.name.wrappedValue,
             description: s.description.wrappedValue ?? ""
         )
         
-        return .init(initial: settings, currentValues: settings, server: s)
+        initial = settings
+        currentValues = settings
+        _server = s
     }
     
     var body: some View {
         List {
             Section("Server Icon") {
                 VStack {
-                    switch currentValues.icon {
-                        case .remote(let file):
-                            if let file = file {
-                                AnyView(LazyImage(source: .file(file), height: 48, width: 48, clipTo: Circle()))
-                            } else {
-                                AnyView(ZStack(alignment: .center) {
-                                    let firstChar = server.name.first!
-                                    
+                    Button {
+                        showIconPhotoPicker = true
+                    } label: {
+                        switch currentValues.icon {
+                            case .remote(let file):
+                                if let file {
+                                    LazyImage(source: .file(file), height: 64, width: 64, clipTo: Circle())
+                                } else {
+                                    ZStack(alignment: .center) {
+                                        let firstChar = server.name.first!
+                                        
+                                        Circle()
+                                            .fill(.gray)  // TODO: background3
+                                            .frame(width: 64, height: 64)
+                                        
+                                        Text(verbatim: "\(firstChar)")
+                                            .font(.title)
+                                    }
+                                }
+                            case .local(let photo):
+                                if let photo {
+                                    LazyImage(source: .local(photo.content), height: 64, width: 64, clipTo: Circle())
+                                } else {
                                     Circle()
-                                        .fill(.gray)  // TODO: background3
-                                        .frame(width: 48, height: 48)
-                                    
-                                    Text(verbatim: "\(firstChar)")
-                                        .font(.title2)
-                                })
-                            }
-                        case .local(let data):
-                            LazyImage(source: .local(data), height: 48, width: 48, clipTo: Circle())
+                                        .foregroundStyle(viewState.theme.background2)
+                                        .frame(width: 64, height: 64)
+                                }
+                        }
                     }
-                }
-                .onTapGesture { showIconPhotoPicker = true }
-                .photosPicker(isPresented: $showIconPhotoPicker, selection: $serverIconPhoto)
-                .onChange(of: serverIconPhoto) { (_, new) in
-                    Task {
-                        if let photo = new {
-                            if let data = try? await photo.loadTransferable(type: Data.self) {
-                                currentValues.icon = .local(data)
+                    .photosPicker(isPresented: $showIconPhotoPicker, selection: $serverIconPhoto)
+                    .onChange(of: serverIconPhoto) { (_, new) in
+                        Task {
+                            if let photo = new {
+                                if let data = try? await photo.loadTransferable(type: Data.self) {
+                                    currentValues.icon = .local(LocalFile(content: data, filename: "icon.\(photo.supportedContentTypes[0].preferredFilenameExtension!)"))  // TODO: figure out filename
+                                }
                             }
                         }
                     }
+                    
+                    Button {
+                        currentValues.icon = .local(nil)
+                    } label: {
+                        Text("Remove")
+                            .font(.caption)
+                            .foregroundStyle(viewState.theme.foreground2)
+                    }
                 }
-                
+            }
+            .listRowBackground(viewState.theme.background)
+            
+            Section("Server Banner") {
+                VStack(spacing: 16) {
+                    Button {
+                        showBannerPhotoPicker = true
+                    } label: {
+                        switch currentValues.banner {
+                            case .remote(let file):
+                                if let file {
+                                    LazyImage(source: .file(file), height: 160, clipTo: RoundedRectangle(cornerRadius: 12))
+                                } else {
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .frame(height: 160)
+                                        .foregroundStyle(viewState.theme.background2)
+                                }
+                            case .local(let photo):
+                                if let photo {
+                                    LazyImage(source: .local(photo.content), height: 160, clipTo: RoundedRectangle(cornerRadius: 12))
+                                } else {
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .frame(height: 160)
+                                        .foregroundStyle(viewState.theme.background2)
+                                }
+                        }
+                    }
+                    .photosPicker(isPresented: $showBannerPhotoPicker, selection: $serverBannerPhoto)
+                    .onChange(of: serverBannerPhoto) { (_, new) in
+                        Task {
+                            if let photo = new {
+                                if let data = try? await photo.loadTransferable(type: Data.self) {
+                                    currentValues.banner = .local(LocalFile(content: data, filename: "banner.\(photo.supportedContentTypes[0].preferredFilenameExtension!)"))  // TODO: figure out filename
+                                }
+                            }
+                        }
+                    }
+                    
+                    Button {
+                        currentValues.banner = .local(nil)
+                    } label: {
+                        Text("Remove")
+                            .font(.caption)
+                            .foregroundStyle(viewState.theme.foreground2)
+                    }
+                }
             }
             .listRowBackground(viewState.theme.background)
             
@@ -90,7 +164,7 @@ struct ServerOverviewSettings: View {
             .listRowBackground(viewState.theme.background2)
             
             Section("Server Description") {
-                TextField(text: $currentValues.description) {
+                TextField(text: $currentValues.description, axis: .vertical) {
                     Text("Add a topic...")
                 }
             }
@@ -117,9 +191,30 @@ struct ServerOverviewSettings: View {
                                 edits.name = currentValues.name
                             }
                             
-                            if currentValues.icon != initial.icon, case .local(let data) = currentValues.icon {
-                                let file = try! await viewState.http.uploadFile(data: data, name: "", category: .icon).get()
-                                edits.icon = file.id
+                            if currentValues.icon != initial.icon {
+                                switch currentValues.icon {
+                                    case .local(.some(let photo)):
+                                        let file = try! await viewState.http.uploadFile(data: photo.content, name: photo.filename, category: .icon).get()
+                                        edits.icon = file.id
+                                    case .local(.none):
+                                        edits.remove = edits.remove ?? []
+                                        edits.remove!.append(.icon)
+                                    default:
+                                        ()
+                                }
+                            }
+                            
+                            if currentValues.banner != initial.banner {
+                                switch currentValues.banner {
+                                    case .local(.some(let photo)):
+                                        let file = try! await viewState.http.uploadFile(data: photo.content, name: photo.filename, category: .banner).get()
+                                        edits.banner = file.id
+                                    case .local(.none):
+                                        edits.remove = edits.remove ?? []
+                                        edits.remove!.append(.banner)
+                                    default:
+                                        ()
+                                }
                             }
                             
                             if currentValues.description != initial.description {
@@ -130,13 +225,14 @@ struct ServerOverviewSettings: View {
                             
                             initial = currentValues
                             showSaveButton = false
+                            serverIconPhoto = nil
+                            serverBannerPhoto = nil
                         }
                     } label: {
                         Text("Save")
                             .foregroundStyle(viewState.theme.accent)
                     }
                 }
-                
             }
         }
         .toolbarBackground(viewState.theme.topBar.color, for: .automatic)
@@ -149,11 +245,11 @@ struct ServerOverviewSettings: View {
 
 
 #Preview {
-    let viewState = ViewState.preview()
-    let server = viewState.servers["0"]!
+    @StateObject var viewState = ViewState.preview()
+    let server = Binding($viewState.servers["0"])!
     
     return NavigationStack {
-        ServerOverviewSettings.fromState(viewState: viewState, server: .constant(server))
+        ServerOverviewSettings(server: server)
     }
     .applyPreviewModifiers(withState: viewState)
 }
